73d11d852e7d7ccce13d95bf59e585aa3c84275f
Owen Prendergast <prendergasto@gmail.com>
2020-02-03 13:29:33 -0800

    basic about me framework created

---
+++ about-me/aboutMe.js
@@ -0,0 +1,12 @@
+const codyDiv = document.getElementById('cody');
+const owenDiv = document.getElementById('owen');
+const joshDiv = document.getElementById('josh');
+const mikeyDiv = document.getElementById('mikey');
+const chrisDiv = document.getElementById('chris');
+
+// going to add event listeners to each of our avatars to reveal a short bio about each one of us
+
+// function readAboutMeBio() {
+//     codyDiv.addEventListener('click', function () {
+
+//     });
