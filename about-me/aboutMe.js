949b78f8a50d19e7a10cd57536e0843a3d1684f5
Owen Prendergast <prendergasto@gmail.com>
2020-02-03 14:42:26 -0800

    added a hover feature to reveal our bios

--- about-me/aboutMe.js
+++ about-me/aboutMe.js
@@ -4,6 +4,47 @@ const joshDiv = document.getElementById('josh');
 const mikeyDiv = document.getElementById('mikey');
 const chrisDiv = document.getElementById('chris');
 
+const bioContainer = document.getElementById('about-me-bio');
+
+
+
+function revealBio() {
+    const showBio = document.getElementById('show-bio');
+    if (showBio.style.display === 'none') {
+        showBio.style.display = 'block';
+    } else {
+        showBio.style.display = 'none';
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// bioContainer.addEventListener('click', showBio);
+
+// function showBio(hidden) {
+//     const revealBio = document.getElementsByClassName('hidden');
+//     if (revealBio[0].offsetWidth > 0 && revealBio[0].offsetHeight > 0) {
+//         revealBio[0].style.visibility = 'visible';
+//     }
+
+// } 
+
+
+
+// function showBio() {
+//     bioContainer.classList.remove('hidden');
+// }
+
 // going to add event listeners to each of our avatars to reveal a short bio about each one of us
 
 // function readAboutMeBio() {
