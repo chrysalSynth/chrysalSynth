3e9f011f5dddea1d007417eed76cb1730431b82d
Michael Romay <amerror@gmail.com>
2020-02-05 16:31:59 -0800

    added effects

--- synth/synth.js
+++ synth/synth.js
@@ -89,11 +89,57 @@ document.addEventListener('DOMContentLoaded', function(event) {
         // '55': 932.327523036179832, //7 - A#
         // '85': 987.766602512248223,  //U - B
     };
+
+
+    //CONVOLVER EFFECT
+    let convolverTime = 3;
+    let convolverEffect = (function() {
+        let convolver = audioCtx.createConvolver(),
+            noiseBuffer = audioCtx.createBuffer(2, convolverTime * audioCtx.sampleRate, audioCtx.sampleRate),
+            left = noiseBuffer.getChannelData(0),
+            right = noiseBuffer.getChannelData(1);
+        for (let i = 0; i < noiseBuffer.length; i++) {
+            left[i] = Math.random() * 2 - 1;
+            right[i] = Math.random() * 2 - 1;
+        }
+        convolver.buffer = noiseBuffer;
+        return convolver;
+    })();
+
+    //BIT CRUSHER EFFECT
+    //USE bits AND normFreq TO CHANGE BIT RATE AND NORM FREQ
+    let bufferSize = 4096;
+    let bits = [1, 4, 8, 16];
+    let normFreq = [0.1, 0.2, 0.5, 1.0];
+    let bitcrushEffect = (function() {
+        let node = audioCtx.createScriptProcessor(bufferSize, 1, 1);
+        node.bits = bits[1]; // between 1 and 16
+        node.normfreq = normFreq[0]; // between 0.0 and 1.0
+        let step = Math.pow(1 / 2, node.bits);
+        let phaser = 0;
+        let last = 0;
+        node.onaudioprocess = function(e) {
+            let input = e.inputBuffer.getChannelData(0);
+            let output = e.outputBuffer.getChannelData(0);
+            for (let i = 0; i < bufferSize; i++) {
+                phaser += node.normfreq;
+                if (phaser >= 1.0) {
+                    phaser -= 1.0;
+                    last = step * Math.floor(input[i] / step + 0.5);
+                }
+                output[i] = last;
+            }
+        };
+        return node;
+    })();
+   
   
     //CONNECTIONS
 
     gain.connect(filterLP);
-    filterLP.connect(compressor);
+    filterLP.connect(bitcrushEffect);
+    bitcrushEffect.connect(convolverEffect);
+    convolverEffect.connect(compressor);
     compressor.connect(myOscilloscope);
     myOscilloscope.connect(audioCtx.destination);
     
